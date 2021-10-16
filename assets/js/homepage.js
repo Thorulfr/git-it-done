3b801a5a9ab0d6eb6fcf8169c7fcb4ea6d46dcd1
Ben H <deepfreespace@gmail.com>
2021-10-15 23:25:30 -0600

    Expanded fetch request to use parameters

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // Format the GitHub API URL
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+    // Make fetch request to URL
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
